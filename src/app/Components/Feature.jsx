9e5896cfd91f14b2e93d63a123b573d5d604f80e
Araya Alejo <82895574+alejoaraya@users.noreply.github.com>
2024-01-08 16:38:26 -0300

    Update Feature.jsx

--- src/app/Components/Feature.jsx
+++ src/app/Components/Feature.jsx
@@ -1,12 +1,16 @@
 /** @format */
 
-import React from "react";
-
 const Feature = () => {
-	return (
-		<div>
-			<h1>Feature</h1>
-		</div>
-	);
+  return (
+    <div className='flex flex-col items-center h-screen gap-5 '>
+      <div className='flex flex-col gap-5 text-center'>
+        <h1 className='text-2xl font-bold'>¿Por que elegir RST-Assist?</h1>
+        <h2 className='w-[350px]'>
+          ChatBot ofrece velocidad, confiabilidad, atencion, personalizacion y
+          otras cosas mas.
+        </h2>
+      </div>
+    </div>
+  );
 };
 export default Feature;
