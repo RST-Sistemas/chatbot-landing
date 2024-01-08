d1fc38a0699536b11bb60b90e31e7544eeb2d047
Araya Alejo <82895574+alejoaraya@users.noreply.github.com>
2024-01-08 16:04:07 -0300

    chance font and delete padding

--- src/app/page.js
+++ src/app/page.js
@@ -12,7 +12,7 @@ import {
 
 export default function Home() {
   return (
-    <main className='flex flex-col items-center justify-between min-h-screen p-24'>
+    <main className='flex flex-col items-center justify-between min-h-screen '>
       <Hero />
       <PriceTable />
       <Testimonials />
