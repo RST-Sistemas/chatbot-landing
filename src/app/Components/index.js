835da70b0fbb48ff5d162011db5a6ad5fb32a7f0
Araya Alejo <82895574+alejoaraya@users.noreply.github.com>
2024-01-08 14:51:37 -0300

    add barrel

---
+++ src/app/Components/index.js
@@ -0,0 +1,8 @@
+export { default as Contact } from "./Contact";
+export { default as FAQ } from "./FAQ";
+export { default as Feature } from "./Feature";
+export { default as Footer } from "./Footer";
+export { default as Hero } from "./Hero";
+export { default as Navbar } from "./Navbar";
+export { default as PriceTable } from "./PriceTable";
+export { default as Testimonials } from "./Testimonials";
--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,25 @@
 /** @format */
-import Hero from "@/app/Components/Hero";
-import FAQ from "@/app/Components/FAQ";
-import Footer from "@/app/Components/Footer";
-import Feature from "@/app/Components/Feature";
-import Testimonials from "@app/Components/Testimonials";
-import PriceTable from "@/app/Components/PriceTable";
-import Contact from "@/app/Components/Contact";
+
+import {
+  Contact,
+  FAQ,
+  Feature,
+  Footer,
+  Hero,
+  PriceTable,
+  Testimonials,
+} from "@/app/Components";
 
 export default function Home() {
-	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<Hero />
-			<PriceTable />
-			<Testimonials />
-			<Feature />
-			<FAQ />
-			<Contact />
-			<Footer />
-		</main>
-	);
+  return (
+    <main className='flex flex-col items-center justify-between min-h-screen p-24'>
+      <Hero />
+      <PriceTable />
+      <Testimonials />
+      <Feature />
+      <FAQ />
+      <Contact />
+      <Footer />
+    </main>
+  );
 }
