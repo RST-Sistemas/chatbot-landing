bbc009c2ac7d74ba8d2edbe2098ff793e6784716
Araya Alejo <82895574+alejoaraya@users.noreply.github.com>
2024-01-08 16:03:05 -0300

    config tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,57 +4,69 @@
  */
 
 module.exports = {
-	content: [
-		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-		"./node_modules/flowbite/**/*.js",
-	],
-	darkMode: "class",
-	theme: {
-		extend: {
-			backgroundImage: {
-				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-				"gradient-conic":
-					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-			},
-		},
-		fontFamily: {
-			body: [
-				"Inter",
-				"ui-sans-serif",
-				"system-ui",
-				"-apple-system",
-				"system-ui",
-				"Segoe UI",
-				"Roboto",
-				"Helvetica Neue",
-				"Arial",
-				"Noto Sans",
-				"sans-serif",
-				"Apple Color Emoji",
-				"Segoe UI Emoji",
-				"Segoe UI Symbol",
-				"Noto Color Emoji",
-			],
-			sans: [
-				"Inter",
-				"ui-sans-serif",
-				"system-ui",
-				"-apple-system",
-				"system-ui",
-				"Segoe UI",
-				"Roboto",
-				"Helvetica Neue",
-				"Arial",
-				"Noto Sans",
-				"sans-serif",
-				"Apple Color Emoji",
-				"Segoe UI Emoji",
-				"Segoe UI Symbol",
-				"Noto Color Emoji",
-			],
-		},
-		plugins: [],
-	},
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#E8C92B",
+          secondary: "#FFF7CE",
+          accent: "#2E2E2E",
+          success: "#2FAC59",
+        },
+      },
+    ],
+  },
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/flowbite/**/*.js",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+    fontFamily: {
+      body: [
+        "Inter",
+        "ui-sans-serif",
+        "system-ui",
+        "-apple-system",
+        "system-ui",
+        "Segoe UI",
+        "Roboto",
+        "Helvetica Neue",
+        "Arial",
+        "Noto Sans",
+        "sans-serif",
+        "Apple Color Emoji",
+        "Segoe UI Emoji",
+        "Segoe UI Symbol",
+        "Noto Color Emoji",
+      ],
+      sans: [
+        "Inter",
+        "ui-sans-serif",
+        "system-ui",
+        "-apple-system",
+        "system-ui",
+        "Segoe UI",
+        "Roboto",
+        "Helvetica Neue",
+        "Arial",
+        "Noto Sans",
+        "sans-serif",
+        "Apple Color Emoji",
+        "Segoe UI Emoji",
+        "Segoe UI Symbol",
+        "Noto Color Emoji",
+      ],
+    },
+  },
 };
