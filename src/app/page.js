225fa8bd6491152b65e468d144855c5dfb8cf9bd
RSTArgentina <rstargentina.sistemas@gmail.com>
2024-01-10 16:54:11 -0300

    Feature

--- src/app/page.js
+++ src/app/page.js
@@ -1,28 +1,28 @@
 /** @format */
 
 import {
-  Contact,
-  FAQ,
-  Feature,
-  Hero,
-  PriceTable,
-  Testimonials,
+	Contact,
+	FAQ,
+	Feature,
+	Hero,
+	PriceTable,
+	Testimonials,
 } from "@/app/Components";
 
 export default function Home() {
-  return (
-    <main className='flex flex-col items-center justify-between min-h-screen '>
-      <Hero />
-      <div className='px-10 md:px-22 lg:px-44'>
-        <PriceTable />
-        <hr className='my-10' />
-        <Testimonials />
-        <hr className='my-10' />
-        <Feature />
-        <hr className='my-10' />
-        <FAQ />
-        <Contact />
-      </div>
-    </main>
-  );
+	return (
+		<main className="flex flex-col items-center justify-between  ">
+			<Hero />
+			<div className="px-10 md:px-22 lg:px-44">
+				<PriceTable />
+				<hr className="my-10" />
+				<Testimonials />
+				<hr className="my-10" />
+				<Feature />
+				<hr className="my-10" />
+				<FAQ />
+				<Contact />
+			</div>
+		</main>
+	);
 }
