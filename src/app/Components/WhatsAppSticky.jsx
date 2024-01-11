7064842ad68f23ec58c1c4b79a2312aaea226b8a
Araya Alejo <82895574+alejoaraya@users.noreply.github.com>
2024-01-11 18:04:35 -0300

    Fix WhatsappSticky

--- src/app/Components/WhatsAppSticky.jsx
+++ src/app/Components/WhatsAppSticky.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function WhatsAppSticky() {
   return (
-    <div className='sticky bottom-0 z-10'>
+    <div className='sticky bottom-0 z-30'>
       <Link href={"https://wa.me/2615086780"} target='_blank'>
         <div className='absolute bottom-5  right-5 flex items-center justify-center w-16 h-16 rounded-full bg-[#25D366] '>
           <Image
