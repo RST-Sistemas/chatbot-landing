35997ce309e4842911a70dee0887810bdef1b3b0
RSTArgentina <rstargentina.sistemas@gmail.com>
2024-01-11 15:13:32 -0300

    asddas
    adsdas

--- src/app/Components/PriceTable/PriceTable.jsx
+++ src/app/Components/PriceTable/PriceTable.jsx
@@ -109,7 +109,7 @@ const PriceTable = () => {
 						<div className="flex justify-center">
 							<Link
 								href="#contact"
-								className="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900 mt-5">
+								className="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900 mt-10">
 								<ButtonContactar className="flex flex-col justify-center" />
 							</Link>
 						</div>
