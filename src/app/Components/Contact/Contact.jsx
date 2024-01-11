daf0caedcf73aaa7cca20121bd2df33fba5ab7ad
RSTArgentina <rstargentina.sistemas@gmail.com>
2024-01-11 15:04:15 -0300

    asdass
    asdasds

--- src/app/Components/Contact/Contact.jsx
+++ src/app/Components/Contact/Contact.jsx
@@ -6,40 +6,42 @@ import Image from "next/image";
 import RobotContact from "../../../../public/assets/image/robot-contact.svg";
 
 const Contact = () => {
-  return (
-    <div className='flex flex-col items-center w-full h-full gap-5 p-5 mt-10 border rounded-lg shadow md:px-24'>
-      <h1 className='text-2xl font-bold text-accent'>CONTACTANOS</h1>
-      <div className='w-full'>
-        <div className='flex flex-wrap justify-between '>
-          <Input label={"Nombre completo"} placeholder={"Bonnie Green"} />
-          <Input label={"Email"} placeholder={"bonniegreen@gmail.com"} />
-        </div>
-        <div className='flex flex-wrap justify-between '>
-          <Input label={"Empresa"} placeholder={"BonnieSRL"} />
-          <Input label={"Rubro empresa"} placeholder={"Venta de celulares"} />
-        </div>
-        <div className='flex flex-col'>
-          <label className='form-control'>
-            <div className='label'>
-              <span className='label-text text-accent'>Mensaje</span>
-            </div>
-            <textarea className='h-24 textarea textarea-bordered'></textarea>
-          </label>
-        </div>
-      </div>
+	return (
+		<div
+			className="flex flex-col items-center w-full h-full gap-5 p-5 mt-10 border rounded-lg shadow md:px-24"
+			id="contact">
+			<h1 className="text-2xl font-bold text-accent">CONTACTANOS</h1>
+			<div className="w-full">
+				<div className="flex flex-wrap justify-between ">
+					<Input label={"Nombre completo"} placeholder={"Bonnie Green"} />
+					<Input label={"Email"} placeholder={"bonniegreen@gmail.com"} />
+				</div>
+				<div className="flex flex-wrap justify-between ">
+					<Input label={"Empresa"} placeholder={"BonnieSRL"} />
+					<Input label={"Rubro empresa"} placeholder={"Venta de celulares"} />
+				</div>
+				<div className="flex flex-col">
+					<label className="form-control">
+						<div className="label">
+							<span className="label-text text-accent">Mensaje</span>
+						</div>
+						<textarea className="h-24 textarea textarea-bordered"></textarea>
+					</label>
+				</div>
+			</div>
 
-      <TyC />
+			<TyC />
 
-      <button className='flex flex-row items-center w-full text-white shadow-md btn btn-primary'>
-        Contactar
-      </button>
-      <Image
-        src={RobotContact}
-        alt='robot-contact'
-        className='absolute hidden mt-32 left-14 lg:inline'
-      />
-    </div>
-  );
+			<button className="flex flex-row items-center w-full text-white shadow-md btn btn-primary">
+				Contactar
+			</button>
+			<Image
+				src={RobotContact}
+				alt="robot-contact"
+				className="absolute hidden mt-32 left-14 lg:inline"
+			/>
+		</div>
+	);
 };
 
 export default Contact;
--- src/app/Components/Feature/Feature.jsx
+++ src/app/Components/Feature/Feature.jsx
@@ -7,6 +7,7 @@ import excel from "@/../../public/assets/svg/excel.svg";
 import userexp from "@/../../public/assets/svg/userexp.svg";
 import shopper from "@/../../public/assets/svg/shopper.svg";
 import Button from "@/app/Components/Buttons/ButtonAssist";
+import Link from "next/link";
 
 const Feature = () => {
 	return (
@@ -119,7 +120,11 @@ const Feature = () => {
 			</div>
 
 			<div className="flex flex-col items-center">
-				<Button />
+				<Link
+					href="#contact"
+					className="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900 my-1">
+					<Button />
+				</Link>
 			</div>
 		</div>
 	);
