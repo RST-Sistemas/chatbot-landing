62b579e90424a548bb1f8740dda3655c45713e61
RSTArgentina <rstargentina.sistemas@gmail.com>
2024-01-12 13:53:04 -0300

    Navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,34 +1,34 @@
 /** @format */
 
 import {
-  Contact,
-  FAQ,
-  Feature,
-  Hero,
-  Navbar,
-  PriceTable,
-  Testimonials,
+	Contact,
+	FAQ,
+	Feature,
+	Hero,
+	Navbar,
+	PriceTable,
+	Testimonials,
 } from "@/app/Components";
 import WhatsAppSticky from "./Components/WhatsAppSticky";
 
 export default function Home() {
-  return (
-    <main className='flex flex-col items-center justify-between scroll-smooth focus:scroll-auto'>
-      <Hero />
-      <div>
-        <Navbar />
-        <div id='feature' className='px-10 md:px-22 lg:px-44'>
-          <Feature />
-          <hr id='price-table' className='my-10' />
-          <PriceTable />
-          <hr id='testimonials' className='my-10' />
-          <Testimonials />
-          <hr id='faq' className='my-10' />
-          <FAQ />
-          <Contact />
-        </div>
-        <WhatsAppSticky />
-      </div>
-    </main>
-  );
+	return (
+		<main className="flex flex-col items-center justify-between scroll-smooth focus:scroll-auto">
+			<div>
+				<Navbar />
+				<Hero />
+				<div id="feature" className="px-10 md:px-22 lg:px-44">
+					<Feature />
+					<hr id="price-table" className="my-10" />
+					<PriceTable />
+					<hr id="testimonials" className="my-10" />
+					<Testimonials />
+					<hr id="faq" className="my-10" />
+					<FAQ />
+					<Contact />
+				</div>
+				<WhatsAppSticky />
+			</div>
+		</main>
+	);
 }
